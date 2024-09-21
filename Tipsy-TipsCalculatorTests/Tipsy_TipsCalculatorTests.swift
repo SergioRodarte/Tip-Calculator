898c8cf259ba7ae7c061fa3017b9ce01ef679f6a
Sergio Rodarte <sergio_isra@hotmail.com>
2024-09-21 01:51:00 -0600

    Initial Commit

---
+++ Tipsy-TipsCalculatorTests/Tipsy_TipsCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tipsy_TipsCalculatorTests.swift
+//  Tipsy-TipsCalculatorTests
+//
+//  Created by Sergio Rodarte on 21/09/24.
+//
+
+import Testing
+@testable import Tipsy_TipsCalculator
+
+struct Tipsy_TipsCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
